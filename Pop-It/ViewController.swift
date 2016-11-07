3f960a41285d8b8f4579ef993a931cb445ff7312
Harvey Chui <chuiharvey@gmail.com>
2016-11-06 23:14:48 -0800

    Initial Commit

---
+++ Pop-It/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pop-It
+//
+//  Created by Harvey Chui on 11/6/16.
+//  Copyright © 2016 Pokemon Kno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
